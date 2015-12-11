1f4377b9e86cf9fdbd72ba14b400c48c1d0428c8
Chris Blow <chris@meedan.com>
2015-12-11 12:35:09 -0800

    Move share config to settings, use dot notation in query string parsing

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -7,36 +7,3 @@
 //= require "views/meme-editor"
 //= require "helpers/font-monitor"
 //= require "vendor/share-button.min"
-
-shareConfig = {
-  description: "Debunk description",
-  image: "https://debunk-meme.herokuapp.com/images/false.png",
-  ui: {
-    flyout: "top right",
-    buttonText: "Share on Facebook or Twitter"
-  },
-  networks: {
-    facebook: {
-      load_sdk: true,
-      caption: "Debunk title",
-      image: "https://debunk-meme.herokuapp.com/images/false.png"
-    },
-    googlePlus: {
-      enabled: false
-    },
-    linkedin: {
-      enabled: false
-    },
-    pinterest: {
-      enabled: false
-    },
-    reddit: {
-      enabled: false
-    },
-    email: {
-      enabled: false
-    }
-  }
-};
-
-var share = new ShareButton(".share-button", shareConfig);
